fe4e2899056b1330c99e8fb56c2d971a28988d0f
github@imacrazyguy.xyz <github@imacrazyguy.xyz>
2022-10-13 11:31:53 -0400

    some stuff

---
+++ src/toggleTheme.jsx
@@ -0,0 +1,22 @@
+const { React, ReactDOM } = require('reactjs');
+
+const root = ReactDOM.createRoot(document.getElementById('themeToggleContainer'));
+
+class toggleTheme extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dark: true,
+        };
+
+        render() {
+            return(
+                <button
+                    className='themeToggleButton'
+                    onClick={() => this.state.dark = !this.state.dark}
+                ></button>
+                //button should have custom svg based on background colors
+            );
+        }
+    }
+}
