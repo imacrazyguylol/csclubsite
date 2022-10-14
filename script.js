5180606525193a5d291834fa826c38caafc52b44
William Rowell <85774751+imacrazyguylol@users.noreply.github.com>
2022-10-14 16:05:16 +0000

    yeah yeah

---
+++ script.js
@@ -0,0 +1,22 @@
+var slideCounter = 1;
+showSlides(slideCounter);
+console.log("anything");
+
+function moveSlide(num) {
+  slideCounter += num
+  showSlides(slideCounter);
+}
+function showSlides(n) {
+  let i;
+  var slides = document.getElementByClassName("slides");
+  if(n > slides.length) {
+    slideCounter = 1;
+  }
+  if(n < 1) {
+    slideCounter = slides.length;
+  }
+  for(i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideCounter-1].style.display = "block";
+}
