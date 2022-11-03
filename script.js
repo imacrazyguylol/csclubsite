41693dfbfd433dcc3b284fa0c988bc91c6d0099b
William Rowell <85774751+imacrazyguylol@users.noreply.github.com>
2022-11-03 00:33:13 +0000

    errereerererer

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ showSlides(slideCounter);
 console.log("anything");
 
 function moveSlide(num) {
-  slideCounter += num
-  showSlides(slideCounter);
+    slideCounter += num
+    showSlides(slideCounter);
 }
 function showSlides(n) {
-  let i;
-  var slides = document.getElementsByClassName("slides");
-  if(n > slides.length) {
-    slideCounter = 1;
-  }
-  if(n < 1) {
-    slideCounter = slides.length;
-  }
-  for(i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideCounter-1].style.display = "block";
+    let i;
+    var slides = document.getElementsByClassName("slides");
+    if (n > slides.length) {
+        slideCounter = 1;
+    }
+    if (n < 1) {
+        slideCounter = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideCounter - 1].style.display = "block";
 }
