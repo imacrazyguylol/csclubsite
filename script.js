68a541c6a526a35152feffc5dd706fec66e94797
Tanjeed Faruque <114783490+Thatonedudefromschool@users.noreply.github.com>
2022-12-02 05:42:32 +0000

    Added the burger menu, added images for projects.html,  navbar is consistent and so is the footer throughout all the pages, animations are working good. Next up to do is the dropdown  for the table of contents, and for the pages to scroll down a bit when first loaded into.

---
+++ script.js
@@ -0,0 +1,56 @@
+let topSlideArr = document.querySelectorAll(".topSlide");
+let leftSlideArr = document.querySelectorAll(".leftSlide");
+let menu = document.getElementById("menuContainer");
+let navBar = document.querySelectorAll(".navBar");
+let miniNav = document.getElementById("miniNav");
+let isMenuClicked = false;
+window.addEventListener('scroll', fadeIn ); 
+function fadeIn() {
+    for (var i = 0; i < topSlideArr.length; i++) {
+        var elem = topSlideArr[i]
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+        if (distInView < 0) {
+            elem.classList.add("visible");
+        } else {
+            elem.classList.remove("visible");
+        }
+    }
+  
+  for(var i = 0; i < leftSlideArr.length; i++) {
+        var elem = leftSlideArr[i];
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+        if(distInView < 0) {
+            elem.classList.add("visible");
+        } else {
+            elem.classList.remove("visible");
+        }
+    }
+  for (var i = 0; i < navBar.length; i++) {
+        var elem = navBar[i]
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 700;
+        if (distInView < 0) {
+            menu.classList.add("visible");
+        } else {
+          menu.classList.remove("visible");
+          if(isMenuClicked) menuFunction(menu);
+        }
+    }
+}
+fadeIn();
+
+function menuFunction(x) {
+  x.classList.toggle("change");
+  if(!isMenuClicked) {
+    miniNav.style.animation = "enlarge 0.7s ease-in-out";
+    miniNav.style.height = "10vh";
+    miniNav.style.width = "32.8vw";
+    miniNav.style.opacity = "1";
+    isMenuClicked = true;
+  } else {
+    miniNav.style.animation = "depress 0.7s ease-in-out";
+    miniNav.style.height = "0vh";
+    miniNav.style.width = "0vw";
+    miniNav.style.opacity = "0";
+    isMenuClicked = false;
+  }
+}
